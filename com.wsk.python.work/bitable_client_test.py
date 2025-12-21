e1635832b6a58544120148e4036c3606e14d2b0f
wsk <571325512@qq.com>
2025-12-21 12:32:07 +0800

    添加新的测试

--- com.wsk.python.work/bitable_client_test.py
+++ com.wsk.python.work/bitable_client_test.py
@@ -1,48 +1,40 @@
-#!/usr/bin/env python3
 """
-冒烟测试脚本
+demo
 """
 from pprint import pprint
 
+from typing import List
 from servyou_bitable.sdk.client import BitableClient
-from servyou_bitable.sdk.model import QueryRecordParam, QueryRecordCondition, UpsertRecordsParam, CreateRecordBody
-
-bitable_server_local = "http://127.0.0.1:8080/bitable"
-# bitable_server_dev = "http://bitable-front.bitable-dev.devops.91lyd.com/bitable"
-# bitable_server_test = "http://bitable-front.bitable-test.sit.91lyd.com/bitable"
-# bitable_server_release = "http://bitable.servyou-release.devops.91lyd.com/bitable"
-# bitable_server_prod = "https://formforge.17win.com/bitable"
-
-app_id = "bitable-collect"
-
-client = BitableClient(bitable_server_local, app_id)
+from servyou_bitable.sdk.model import QueryRecordParam, QueryCondition, UpsertRecordsParam, CreateRecordBody, \
+    QueryRecordResp
+# noinspection PyProtectedMember
+from servyou_bitable.util.table_util import _set_global_context
 
 
 def test_query() -> None:
     # 1. 构造参数
     param = QueryRecordParam(
-        base_id="appvfzEuWsubV",
         table_id="tvvTycVXgvv",
         selectFieldIds=["f42jly257guti"],
-        condition=QueryRecordCondition(
+        condition=QueryCondition(
             logicOp="and",
             childrenConditions=[
-                QueryRecordCondition(
+                QueryCondition(
                     logicOp="or",
                     childrenConditions=[
-                        QueryRecordCondition(
+                        QueryCondition(
                             fieldId="f42jly257guti",
                             compareOp="greater",
                             value=[1]
                         ),
-                        QueryRecordCondition(
+                        QueryCondition(
                             fieldId="f42jly257guti",
                             compareOp="less",
                             value=[0]
                         )
                     ]
                 ),
-                QueryRecordCondition(
+                QueryCondition(
                     fieldId="f42jly257guti",
                     compareOp="notEmpty"
                 )
@@ -56,7 +48,7 @@ def test_query() -> None:
         page_no = 1
         while True:
             param.pageNo = page_no
-            records = client.query_record(param)
+            records: List[QueryRecordResp] = client.query_record(param)
             if not records:  # 空列表就停
                 break
             print(f"第 {page_no} 页，返回条数：{len(records)}")
@@ -64,35 +56,66 @@ def test_query() -> None:
             page_no += 1
 
     except Exception as e:
-        print("❌ 调用失败：", e)
+        print("调用失败：", e)
         raise
 
 
 def test_upsert() -> None:
     # 1. 构造参数
     param = UpsertRecordsParam(
-        base_id="appvfzEuWsubV",
         table_id="tvvTycVXgvv",
         firstBatch=True,
         lastBatch=True,
         records=[
-            CreateRecordBody(fields={"f42jly257guti": 8800888, "id": "我来自于北京111"}),  # 未指定 rowId 则是插入
-            CreateRecordBody(rowId="rec_xxx", fields={"f42jly257guti": 990099, "id": "我来自于上海222"})  # 指定 rowId 即更新
+            CreateRecordBody(fields={"f42jly257guti": 88008, "id": "我来自于北京11"}),  # 未指定 rowId 则是插入
+            CreateRecordBody(rowId="rec_xxx", fields={"f42jly257guti": 99008, "id": "我来自于上海22"})  # 指定 rowId 即更新
         ]
     )
     # 2. 调用
     try:
-        resp = client.upsert_records(param)
+        client.upsert_records(param)
         print("upsert数据全部成功")
 
     except Exception as e:
         print("异常：", e)
 
 
-# 仅在“python -m bitable_sdk.test_run”时执行
+def test_delete() -> None:
+    from servyou_bitable.sdk.model import DeleteRecordParam
+
+    # 1. 按 rowId 删除示例
+    param1 = DeleteRecordParam(
+        table_id="tvvTycVXgvv",
+        rowIds=["r05pm910pa1a"],
+        condition=QueryCondition(
+            fieldId="f42jly257guti",
+            compareOp="equal",
+            value=[8800888]
+        )
+    )
+    try:
+        client.delete_records(param1)
+        print("删除成功")
+    except Exception as e:
+        print("删除异常：", e)
+
+
 if __name__ == "__main__":
+    bitable_server_local = "http://127.0.0.1:8080/bitable"
+    # bitable_server_dev = "http://bitable-front.bitable-dev.devops.91lyd.com/bitable"
+    # bitable_server_test = "http://bitable-front.bitable-test.sit.91lyd.com/bitable"
+    # bitable_server_release = "http://bitable.servyou-release.devops.91lyd.com/bitable"
+    # bitable_server_prod = "https://formforge.17win.com/bitable"
+
+    _set_global_context(base_id="appvfzEuWsubV")
+    _set_global_context(bitable_url=bitable_server_local)
+    client = BitableClient()
+
     print("---------------查询测试--------------")
     test_query()
     print()
     print("---------------upsert测试--------------")
     test_upsert()
+    print()
+    print("---------------delete测试--------------")
+    test_delete()
